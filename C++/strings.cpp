316777223c81d25b2d3576b52b7a2239afa87026
Syed Afrid <83272741+s-afrid@users.noreply.github.com>
2025-07-04 14:39:12 +0000

    strings in c++

---
+++ C++/strings.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    // string is a collection of characters same as array
+    string s = "Hello";
+    cout << s[1] << endl; // Access element by index
+    // to find the length of the string
+    cout << s.size() << endl;
+    cout << s.length() << endl;
+}
